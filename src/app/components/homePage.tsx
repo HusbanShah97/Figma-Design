6597739dbac49ebed3d466b0cc5b87102595aff2
HusbanShah97 <husbanshah97@gmail.com>
2024-11-23 01:35:25 +0500

    navbar setup

--- src/app/components/homePage.tsx
+++ src/app/components/homePage.tsx
@@ -5,28 +5,30 @@ export default function HomePage() {
         
         <div id='Home' className='w-1440 h-1132 pt-590 pl-720 bg-blue-950 '>
 
-            <div id='Contianer' className='w-1322 h-91 bg-green-800'>
-                <nav className='w-1440 h-1132 '>
-                <div className='w-187 h-58   pt-10'>
-                          <h1 className=''>HusbanTech</h1>
-                    </div>
-                  
-    
-                    <div>
-                        <ul className='flex justify-center pt-10 gap-5'>
-                            <li><Link href={'/'}>Home</Link></li>
-                            <li><Link href={'/'}>Product</Link></li>
-                            <li><Link href={'/'}>Pricing</Link></li>
-                            <li><Link href={'/'}>Contact</Link></li>
-                        </ul>
-                    </div>
-
-
-                </nav>
+            <div id='Contianer' className='w-1322 h-91 bg-green-800 ml-14'>
+                <div className=' w-187 h-58 ml-24 pt-10'>
+                    <h1 className='text-2xl'>HusbanTech</h1>
+                </div>
                 
+                <div className='w-815 h-14 ml-364 mt-4'>
+
+                <section>
+                    <ul className='w-72 flex gap-5 bg-yellow-800 '>
+                        <li><Link href={'/'}>Home</Link></li>
+                        <li><Link href={'/product'}>Product</Link></li>
+                        <li><Link href={'/pricing'}>Pricing</Link></li>
+                        <li><Link href={'/contact'}>Contact</Link></li>
+                    </ul>
+                </section>
+
+                <div className='w-48 h-14  gap-12  ml-626 '>
+                    <button type='button'>Login</button>
+                    <button type='button'>JOIN US</button>
+                </div>
+
+                </div>
+
            </div>
-
-
         </div>
 
     )
