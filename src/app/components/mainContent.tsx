fc05ea57791794c079711c4b99c32b87126dfd83
HusbanShah97 <husbanshah97@gmail.com>
2024-11-24 18:56:31 +0500

    complete design

--- src/app/components/mainContent.tsx
+++ src/app/components/mainContent.tsx
@@ -9,7 +9,7 @@ export default function MainContent () {
                 <div id="row" className="w-[701px] h-[496px]  ml-36 mt-40 ">
 
                     <div className="flex justify-center ml-14">
-                    <h5 className="text-blue-500 font-bold">Welcome</h5>
+                    <h5 className="font-bold text-blue-500">Welcome</h5>
                     </div>
 
                     
@@ -18,12 +18,48 @@ export default function MainContent () {
                         
                         <h1 className="text-[58px] font-bold text-center">Selling on the internet like a pro </h1>
                     </div>
-  
-                     
-
+                    
+                     <p className="w-[536px] h-[60px] text-center text-xl font-bold mx-28 my-9">We know how large objects will act, but things on a 
+                     small scale just do not act that way.</p>
+                      
+                    
+                    <div className="w-[365px] h-[52px] flex gap-[12px] ml-44">
+                        <button type="button" className="w-[193px] h-[52px] bg-[#23A6F0] text-[#ffff] rounded-md font-bold">Get Quote Now</button>
+                        <button type="button" className="w-[162px] h-[52px]  border-2 border-[#23A6F0] text-[#23A6F0] font-bold">Learn More</button>
+                               
+                    </div>
 
                 </div>
             
+            
+                <div id="SecondRow" className='w-[1046px] h-[292px]  flex gap-8 bg-slate-800 mt-14' >
+                    
+                    <div className='w-[328px] h-[292px] bg-[#FFFFFF]'>
+                        <section className="w-[70px] h-[76px] bg-[#FFDCD1] my-5 mx-8 rounded-xl"></section>
+                        <h3 className="w-[135px] h-6 text-[#252B42] ml-11 font-bold">training courses</h3>
+                        <div className="w-[50px] h-[2px] bg-[#E74040] mt-5 ml-12"></div>
+                        <p className=" w-[222px] h-[60px] text-[#737373] text-center font-bold ml-10 mt-5">The gradual accumulation of information about atomic and small-scale behaviour...</p>
+                    </div>
+                    
+                    <div className='w-[328px] h-[292px] bg-[#FFFFFF]'>
+                        <section className="w-[70px] h-[76px] bg-[#B9EAA8] my-5 mx-8 rounded-xl"></section>
+                        <h3 className="w-[160px] h-6 text-[#252B42] ml-11 font-bold ">2,769 online courses</h3>
+                        <div className="w-[50px] h-[2px] bg-[#E74040] mt-5 ml-12"></div>
+                        <p className=" w-[222px] h-[60px] text-[#737373] text-center font-bold ml-10 mt-5">The gradual accumulation of information about atomic and small-scale behaviour...</p>
+                    </div>
+
+                    <div className='w-[328px] h-[292px] bg-[#23A6F0]'>
+                        <section className="w-[70px] h-[76px] bg-[#FFFFFF] my-5 mx-8 rounded-xl"></section>
+                        <h3 className="w-[135px] h-6 text-[#FFFFFF] ml-11 font-bold">training ourses</h3>
+                        <div className="w-[50px] h-[2px] bg-[#FFFFFF] mt-5 ml-12"></div>
+                        <p className=" w-[222px] h-[60px] text-[#FFFFFF] text-center font-bold ml-10 mt-5">The gradual accumulation of information about atomic and small-scale behaviour...</p>
+                    </div>
+                    
+
+                    
+                </div>
+                
+                
             </div>
 
       </div>
